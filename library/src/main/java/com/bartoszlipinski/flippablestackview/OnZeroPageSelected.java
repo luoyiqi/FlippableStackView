7e1524581026fbb396b9994af8dbe800e1b99fe9
Aritra Roy <webmaster.aritra93@gmail.com>
2015-12-15 15:43:43 +0530

    Removed OnZeroPageListener

--- library/src/main/java/com/bartoszlipinski/flippablestackview/OnZeroPageSelected.java
+++
@@ -1,9 +0,0 @@
-package com.bartoszlipinski.flippablestackview;
-
-/**
- * Created by aritraroy on 12/8/15.
- */
-public interface OnZeroPageSelected {
-
-    void zeroPageDimens(float x, float y);
-}
